a1f264a183aecb7c0a73f98c3a51ce126248a0ee
studentristan <54915507+studentristan@users.noreply.github.com>
2021-12-19 02:24:40 -0500

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-function defaultTask(cb) {
-  // this is where the code for the default task goes...
-  // I didn't play around with gulp too much because the converter I like to use for SCSS uglifies the css for me,
-  // and I tend to like to leave the HTML as is unless actually producing the site for someone else, in which case 
-  // I'll use a one-time converter. 
-  cb();
-}
-
-exports.default = defaultTask
